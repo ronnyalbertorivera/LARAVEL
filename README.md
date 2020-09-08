7a290c3dc68f222ff411b553524caa3a3b2c42b5
Ronny A. Rivera <ronnyalbertorivera@gmail.com>
2020-09-08 03:12:06 +0000

    Commit Subir archivos Dtely a la Rama frontend_DTELY Base 0a_

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ riverara 07 sep 2020 [1723 hrs]
 Proyecto vacio para
 http://laravel.atatrot.com
 ################################ FIN  001
+
+################################ INICIO 002
+riverara 07 sep 2020 [2223 hrs]
+Proyecto llenado con la Inf de dtely que fue movido aqui
+Lo anterior paso en Hostnger
+http://laravel.atatrot.com
+ACTIVO
+https://github.com/ronnyalbertorivera/laravel.git
+SIGUE VACIO
+################################ FIN  002
---
+++ resources/views/welcome.blade_revB.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="img/DTely_LCF_FaviconLogoTXa.png" />
+        <title>Dtely Firm</title>
+
+        <!--   ############# Fonts #############   -->
+<link href='https://fonts.googleapis.com/css?family=Spectral SC' rel='stylesheet'>
+
+        <!-- ############# Styles Inicio ############# 
+
+<a href="https://blog.laravel.com" style="font-size: 50px; color: #aaaaaa;">Blog</a>
+
+-->
+        <style>
+            html, body {
+                background-color: #000;
+                color: #636b6f;
+		font-family: 'Spectral SC';
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 2.4em;
+                color: #3bf;
+
+            }
+	  .title:hover{
+                font-size: 3.4em;
+                color: white;
+            }
+           .Hfijo {
+                height: 100px;
+            }
+	
+            .links > a {
+                color: #636b6f;
+                padding: 0px 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+	   .links > a:hover {
+                background-color: #111;
+                color: #fff;
+
+            }
+	  .logo:hover{
+ 		width:45px !important;
+            }
+           .override {
+                background-color: transparent !important;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+
+        <!-- ############# Styles Fin ############# -->
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <div class="content">
+           <img src="img/DTely_LCFInstagramLogo.jpg">
+               <p>                Entrando ...  </p>
+               <b>                 ...  Al Desarrollo Web de su espacio      </b>
+               <div class="Hfijo  flex-center">
+               <div class="title m-b-md">
+                   ...  Diseñando  ... 
+                </div>
+                </div>
+ 		<div class="links">
+                    <a class="override" href="#"><spam style="font-size: 50px; color: #aaaaaa;"><img class="logo" src="img/DTely_LCF_FaviconLogo.jpg" width=35px></spam></a>
+                    <a ref="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>      
+            </div>
+        </div>
+    </body>
+</html>
